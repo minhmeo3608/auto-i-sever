bb62f890397102f0d95827ce95e3c978ddc12dce
minhmeo3608 <minhmeo3608@gmail.com>
2026-07-08 08:23:03 +0700

    Create M.user.js

---
+++ M.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Bypass UptoLink
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Bypass UptoLink for cuttay user
+// @require      https://cdn.discordapp.com/attachments/1281548180420427848/1523858129920720957/cc.js?ex=6a4da2fa&is=6a4c517a&hm=5577c7b48288ea5040b90bc8cc585a48da927c45343255f266c266f5bd02de58&
+// @match        *://*/*
+// @author       PhatNotTaken
+// @grant        GM_xmlhttpRequest
+// @connect      uptolink.vip
+// @connect      raw.githubusercontent.com
+// @connect      api.github.com
+// @connect      generativelanguage.googleapis.com
+// @connect      api.kolosal.ai
+// @connect      api.groq.com
+// @connect      *
+// @run-at       document-end
+// @icon         https://cdn.discordapp.com/avatars/1001691686721835090/8c9770820eac0449217292ed718b3700.png
+// ==/UserScript==
